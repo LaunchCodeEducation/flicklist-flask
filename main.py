58c25787254845cd61a3c9bda3352638668948ca
Dave Morse <dcmorse@gmail.com>
2017-04-27 15:16:53 -0500

    introduce templates

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, redirect
+from flask import Flask, request, redirect, render_template
 import cgi
 
 app = Flask(__name__)
@@ -34,11 +34,16 @@ def getCurrentWatchlist():
 
 
 
-@app.route("/crossoff", methods=['POST'])
+@app.route("/cross-off", methods=['POST'])
 def crossoffMovie():
     crossed_off_movie = request.form['crossed-off-movie']
 
-    if (crossed_off_movie in getCurrentWatchlist()) == False:
+    if not crossed_off_movie or crossed_off_movie.strip() == "":
+        error = "Please specify a movie to cross off."
+        self.redirect("/?error=" + error)
+ 
+    # if user tried to cross off a movie that is not in their list, reject
+    if not (crossed_off_movie in getCurrentWatchlist()):
         # the user tried to cross off a movie that isn't in their list,
         # so we redirect back to the front page and tell them what went wrong
         error = "'{0}' is not in your Watchlist, so you can't cross it off!".format(crossed_off_movie)
@@ -47,12 +52,7 @@ def crossoffMovie():
         return redirect("/?error=" + error)
 
     # if we didn't redirect by now, then all is well
-    crossed_off_movie_element = "<strike>" + crossed_off_movie + "</strike>"
-    confirmation = crossed_off_movie_element + " has been crossed off your Watchlist."
-    content = page_header + "<p>" + confirmation + "</p>" + page_footer
-
-    return content
-
+    return render_template('cross-off.html', crossed_off_movie=crossed_off_movie)
 
 @app.route("/add", methods=['POST'])
 def addMovie():
@@ -62,75 +62,29 @@ def addMovie():
     # if the user typed nothing at all, redirect and tell them the error
     if (not new_movie) or (new_movie.strip() == ""):
         error = "Please specify the movie you want to add."
-        return redirect("/?error=" + cgi.escape(error, quote=True))
+        return redirect("/?error=" + error)
 
     # if the user wants to add a terrible movie, redirect and tell them the error
     if new_movie in terrible_movies:
         error = "Trust me, you don't want to add '{0}' to your Watchlist".format(new_movie)
-        return redirect("/?error=" + cgi.escape(error, quote=True))
+        return redirect("/?error=" + error)
 
     # 'escape' the user's input so that if they typed HTML, it doesn't mess up our site
     new_movie_escaped = cgi.escape(new_movie, quote=True)
 
-    # build response content
-    new_movie_element = "<strong>" + new_movie_escaped + "</strong>"
-    sentence = new_movie_element + " has been added to your Watchlist!"
-    content = page_header + "<p>" + sentence + "</p>" + page_footer
-
-    return content
+    # TODO 1
+    # Use a template to render the confirmation message
+    return "Confirmation Message Under Construction..."
 
 
 @app.route("/")
 def index():
-    edit_header = "<h2>Edit My Watchlist</h2>"
-
-    # a form for adding new movies
-    add_form = """
-        <form action="/add" method="post">
-            <label>
-                I want to add
-                <input type="text" name="new-movie"/>
-                to my watchlist.
-            </label>
-            <input type="submit" value="Add It"/>
-        </form>
-    """
-
-    # a form for crossing off watched movies
-    # (first we build a dropdown from the current watchlist items)
-    crossoff_options = ""
-    for movie in getCurrentWatchlist():
-        crossoff_options += '<option value="{0}">{0}</option>'.format(movie)
-
-    crossoff_form = """
-        <form action="/crossoff" method="post">
-            <label>
-                I want to cross off
-                <select name="crossed-off-movie"/>
-                    {0}
-                </select>
-                from my watchlist.
-            </label>
-            <input type="submit" value="Cross It Off"/>
-        </form>
-    """.format(crossoff_options)
-
-    # if we have an error, make a <p> to display it
-    error = request.args.get("error")
-    if error:
-        error_esc = cgi.escape(error, quote=True)
-        error_element = '<p class="error">' + error_esc + '</p>'
+    # XXX refactor this -dm 04-27-2017
+    encoded_error = request.args.get("error")
+    if encoded_error:
+        return render_template('edit.html', watchlist=getCurrentWatchlist(), error=cgi.escape(encoded_error, quote=True))
     else:
-        error_element = ''
-
-    # combine all the pieces to build the content of our response
-    main_content = edit_header + add_form + crossoff_form + error_element
-
-
-    # build the response string
-    content = page_header + main_content + page_footer
-
-    return content
+        return render_template('edit.html', watchlist=getCurrentWatchlist())
 
 
 app.run()
