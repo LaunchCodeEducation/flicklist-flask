f70c01001ac3ce069f9061ab5a8e7e7321894cad
Dave Morse <dcmorse@gmail.com>
2017-04-28 11:54:22 -0500

    check-off -> watched-it rename

--- main.py
+++ main.py
@@ -30,11 +30,11 @@ add_form = """
 """
 
 # a form for crossing off watched movies
-crossoff_form = """
-    <form action="/cross-off" method="post">
+watched_form = """
+    <form action="/watched-it" method="post">
         <label>
             I want to cross off
-            <select name="crossed-off-movie"/>
+            <select name="watched-movie"/>
                 <option value="Star Wars">Star Wars</option>
                 <option value="My Favorite Martian">My Favorite Martian</option>
                 <option value="The Avengers">The Avengers</option>
@@ -47,11 +47,11 @@ crossoff_form = """
 """
 
 
-@app.route("/cross-off", methods=['POST'])
-def crossoffMovie():
-    crossed_off_movie = request.form['crossed-off-movie']
-    crossed_off_movie_element = "<strike>" + crossed_off_movie + "</strike>"
-    confirmation = crossed_off_movie_element + " has been crossed off your Watchlist."
+@app.route("/watched-it", methods=['POST'])
+def watchMovie():
+    watched_movie = request.form['watched-movie']
+    watched_movie_element = "<strike>" + watched_movie + "</strike>"
+    confirmation = watched_movie_element + " has been crossed off your Watchlist."
     content = page_header + "<p>" + confirmation + "</p>" + page_footer
 
     return content
@@ -74,7 +74,7 @@ def index():
     edit_header = "<h2>Edit My Watchlist</h2>"
 
     # build the response string
-    content = page_header + edit_header + add_form + crossoff_form + page_footer
+    content = page_header + edit_header + add_form + watched_form + page_footer
 
     return content
 
