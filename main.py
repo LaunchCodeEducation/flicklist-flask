a4c4ab748fcb6b6da786e8634cbc2a71cf71b089
Dave Morse <dcmorse@gmail.com>
2017-05-03 11:40:42 -0500

    display runtime errors in the browswer

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from flask import Flask, request
 
 app = Flask(__name__)
 
+app.config['DEBUG'] = True      # displays runtime errors in the browser, too
+
 page_header = """
 <!DOCTYPE html>
 <html>
