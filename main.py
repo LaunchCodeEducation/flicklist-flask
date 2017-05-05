0187e43d9cfced5f7adce73ed5cc46b281de2034
Cheryl <cherylschaefer@gmail.com>
2017-05-05 14:19:41 -0500

    added port number to connection string and sql echo

--- main.py
+++ main.py
@@ -4,7 +4,8 @@ import cgi
 
 app = Flask(__name__)
 app.config['DEBUG'] = True      # displays runtime errors in the browser, too
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://flicklist:MyNewPass@localhost/flicklist'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://flicklist:MyNewPass@localhost:8889/flicklist'
+app.config['SQLALCHEMY_ECHO'] = True
 
 db = SQLAlchemy(app)
 
