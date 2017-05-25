db37682055b7fcc5e4be8329c23ea3fd79967502
Sarah Richardson <seraries@gmail.com>
2017-05-25 15:11:10 -0500

    make function name pythonic

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ app.config['DEBUG'] = True      # displays runtime errors in the browser, too
 @app.route("/")
 def index():
     # choose a movie by invoking our new function
-    movie = getRandomMovie()
+    movie = get_random_movie()
 
     # build the response string
     content = "<h1>Movie of the Day</h1>"
@@ -20,7 +20,7 @@ def index():
 
     return content
 
-def getRandomMovie():
+def get_random_movie():
     # TODO: make a list with at least 5 movie titles
     # TODO: randomly choose one of the movies, and return it
     return "The Big Lebowski"
