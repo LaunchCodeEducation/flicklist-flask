e23088d293f4ba1003e058e88d9bcb41b2d395a3
Sarah Richardson <seraries@gmail.com>
2017-05-25 13:19:42 -0500

    Make naming/format consistent with previous studio

--- main.py
+++ main.py
@@ -20,6 +20,41 @@ page_footer = """
 </html>
 """
 
+# a form for adding new movies
+add_form = """
+    <form action="/add" method="post">
+        <label>
+            I want to add
+            <input type="text" name="new-movie"/>
+            to my watchlist.
+        </label>
+        <input type="submit" value="Add It"/>
+    </form>
+"""
+
+def getCurrentWatchlist():
+    # returns user's current watchlist--hard coded for now
+    return [ "Star Wars", "Minions", "Freaky Friday", "My Favorite Martian" ]
+
+# a form for crossing off watched movies
+# (first we build a dropdown from the current watchlist items)
+crossoff_options = ""
+for movie in getCurrentWatchlist():
+    crossoff_options += '<option value="{0}">{0}</option>'.format(movie)
+
+crossoff_form = """
+    <form action="/crossoff" method="post">
+        <label>
+            I want to cross off
+            <select name="crossed-off-movie"/>
+                {0}
+            </select>
+            from my watchlist.
+        </label>
+        <input type="submit" value="Cross It Off"/>
+    </form>
+""".format(crossoff_options)
+
 # a list of movies that nobody should have to watch
 terrible_movies = [
     "Gigli",
@@ -29,28 +64,22 @@ terrible_movies = [
     "Starship Troopers"
 ]
 
-def getCurrentWatchlist():
-    # For now, we are just pretending
-    # returns user's current watchlist
-    return [ "Star Wars", "Minions", "Freaky Friday", "My Favorite Martian" ]
 
+@app.route("/crossoff", methods=['POST'])
+def crossoff_movie():
+    crossed_off_movie = request.form['crossed-off-movie']
 
-
-@app.route("/watched-it", methods=['POST'])
-def watchMovie():
-    watched_movie = request.form['watched-movie']
-
-    if watched_movie not in getCurrentWatchlist():
+    if crossed_off_movie not in getCurrentWatchlist():
         # the user tried to cross off a movie that isn't in their list,
         # so we redirect back to the front page and tell them what went wrong
-        error = "'{0}' is not in your Watchlist, so you can't cross it off!".format(watched_movie)
+        error = "'{0}' is not in your Watchlist, so you can't cross it off!".format(crossed_off_movie)
 
         # redirect to homepage, and include error as a query parameter in the URL
         return redirect("/?error=" + error)
 
     # if we didn't redirect by now, then all is well
-    watched_movie_element = "<strike>" + watched_movie + "</strike>"
-    confirmation = watched_movie_element + " has been crossed off your Watchlist."
+    crossed_off_movie_element = "<strike>" + crossed_off_movie + "</strike>"
+    confirmation = crossed_off_movie_element + " has been crossed off your Watchlist."
     content = page_header + "<p>" + confirmation + "</p>" + page_footer
 
     return content
@@ -60,14 +89,14 @@ def watchMovie():
 def addMovie():
     new_movie = request.form['new-movie']
 
-    # TODO 2
+    # TODO 
+    # 'escape' the user's input so that if they typed HTML, it doesn't mess up our site
+    
+    # TODO 
     # if the user typed nothing at all, redirect and tell them the error
 
-    # TODO 3
-    # if the user wants to add a terrible movie, redirect and tell them the error
-
-    # TODO 1
-    # 'escape' the user's input so that if they typed HTML, it doesn't mess up our site
+    # TODO 
+    # if the user wants to add a terrible movie, redirect and tell them not to add it b/c it sucks
 
     # build response content
     new_movie_element = "<strong>" + new_movie + "</strong>"
@@ -81,37 +110,6 @@ def addMovie():
 def index():
     edit_header = "<h2>Edit My Watchlist</h2>"
 
-    # a form for adding new movies
-    add_form = """
-        <form action="/add" method="post">
-            <label>
-                I want to add
-                <input type="text" name="new-movie"/>
-                to my watchlist.
-            </label>
-            <input type="submit" value="Add It"/>
-        </form>
-    """
-
-    # a form for crossing off watched movies
-    # (first we build a dropdown from the current watchlist items)
-    watch_options = ""
-    for movie in getCurrentWatchlist():
-        watch_options += '<option value="{0}">{0}</option>'.format(movie)
-
-    watch_form = """
-        <form action="/watched-it" method="post">
-            <label>
-                I want to cross off
-                <select name="watched-movie"/>
-                    {0}
-                </select>
-                from my watchlist.
-            </label>
-            <input type="submit" value="Cross It Off"/>
-        </form>
-    """.format(watch_options)
-
     # if we have an error, make a <p> to display it
     error = request.args.get("error")
     if error:
@@ -121,7 +119,7 @@ def index():
         error_element = ''
 
     # combine all the pieces to build the content of our response
-    main_content = edit_header + add_form + watch_form + error_element
+    main_content = edit_header + add_form + crossoff_form + error_element
 
 
     # build the response string
