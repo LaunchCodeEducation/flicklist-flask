1602e46e286717c3044c758529c12c4c7a60a587
Sarah Richardson <seraries@gmail.com>
2017-06-16 13:01:07 -0500

    Add init method

--- models.py
+++ models.py
@@ -5,6 +5,11 @@ class User(db.Model):
     email = db.Column(db.String(120), unique=True)
     password = db.Column(db.String(120))
     movies = db.relationship('Movie', backref='owner')
+    
+    def __init__(self, email, password):
+        self.email = email
+        self.password = password
+    
     def __repr__(self):
         return '<User %r>' % self.email
 
