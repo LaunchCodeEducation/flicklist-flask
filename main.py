38137be6e81b9745cac3a5b4e49e52ecb62dfab9
Sarah Richardson <seraries@gmail.com>
2017-06-16 13:00:05 -0500

    Add init method

--- main.py
+++ main.py
@@ -13,6 +13,10 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True)
     password = db.Column(db.String(120))
+    
+    def __init__(self, email, password):
+        self.email = email
+        self.password = password
 
     def __repr__(self):
         return '<User %r>' % self.email
